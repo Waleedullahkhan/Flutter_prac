a1ac504f744ca9ddc31551bbee53f34256d98b7b
Waleedullahkhan <waleedk343@gmail.com>
2021-09-15 00:01:59 +0500

    prac_2

---
+++ prac2.dart
@@ -0,0 +1,26 @@
+//   void main(){
+//   var List = <int> [50,5,4,88];
+//   List.sort();
+//   var reversedList = new List.from(List.reversed);
+//   print(reversedList);
+// }
+// void main(){
+// var tringList = ['vue', 'kotlin','dart', 'angular', 'flutter'];
+// tringList.sort();
+// print(tringList); 
+// }
+
+// void main(){
+//   var List = <String>["kotlin","Javascript","C++"];
+//   List.sort();
+//   print(List);
+// }
+
+
+
+
+// void main() {   
+// var intList = [0, 5, 2, 3, 8, 17, 11];
+// intList.sort();
+// print(intList);
+// }
